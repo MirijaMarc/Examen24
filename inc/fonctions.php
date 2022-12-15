1c4be8c92e93433396b6faa10f1c857d98224a94
Mirija <mirijarazafimbelo@gmail.com>
2022-12-15 23:17:01 +0300

    getallclients

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -1,7 +1,18 @@
 <?php
+require_once ("connectiom.php");
+function getAllClients(){
+$clients=array();
 
+ for ($i=0; $i < 10 ; $i++) { 
+    #
+    $clients[$i]['email']=$i->email;
+    $clients[$i]['nom']=$i->nom;
+    $clients[$i]['numero']=$i->numero;
+ 
+ }
+ return $clients;
 
-
-
+}
 
 ?>
+
