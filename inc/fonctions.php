2b40b0743bf35a8fcdd5073ca5ccbc0c26024c5b
Mirija <mirijarazafimbelo@gmail.com>
2022-12-16 00:19:40 +0300

    script

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -1,10 +1,27 @@
 <?php
+require_once ("connectiom.php");
+function getAllClients(){
+$clients=array();
+
+ for ($i=0; $i < 10 ; $i++) { 
+    #
+    $clients[$i]['email']=$i->email;
+    $clients[$i]['nom']=$i->nom;
+    $clients[$i]['numero']=$i->numero;
  
- function insertTohabitation($connection,$idType, $nbrChambre, $loyer, $idQuartier){
-    $sql = sprintf("INSERT INTO Habitation VALUES (%s,%)")
  }
+ return $clients;
 
+}
+function chekingpassword($pasword1,$pasword2){
+  if (strcmp($pasword1,$pasword2)==1) {
+    return true;
+    }
+    return false;
+}
+function getAllhabitation($habitation,$log1,$log2,$log3){
+ $habitation =array($log1,$log2,$log3);
 
-
-
+return $habitation;
+}
 ?>
