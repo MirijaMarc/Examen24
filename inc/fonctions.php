e2bbf8ffa872446d6fcba31baf3551eb78faa1d9
Mirija <mirijarazafimbelo@gmail.com>
2022-12-16 00:01:11 +0300

    getallhab

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -13,6 +13,15 @@ $clients=array();
  return $clients;
 
 }
+function chekingpassword($pasword1,$pasword2){
+  if (strcmp($pasword1,$pasword2)==1) {
+    return true;
+    }
+    return false;
+}
+function getAllhabitation($habitation,$log1,$log2,$log3){
+ $habitation =array($log1,$log2,$log3);
 
+return $habitation;
+}
 ?>
-
