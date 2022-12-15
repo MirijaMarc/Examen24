999f6685c3cf2e82aea5ffdf475a6a723645e5ff
Mirija <mirijarazafimbelo@gmail.com>
2022-12-16 00:41:39 +0300

    update sql

--- script.sql
+++ script.sql
@@ -2,23 +2,18 @@ create database hotel;
 
 \c hotel;
 
-create sequence typeHseq start with 1;
-create sequence quartierseq start with 1;
-create sequence habitationseq; start with 1
-create sequence picsseq start with 1;
-create sequence clientseq start with 1;
-create sequence adminseq start with 1;
-
-
 create table TypeH(
     idTypeH varchar(15) primary key,
     typeHabitation varchar(30)
 );
+create sequence typeHseq start with 1;
 
 create table Quartier(
     idQuartier varchar(15) primary key,
     quartier varchar(30)
 );
+create sequence quartierseq start with 1;
+
 
 create table Habitation(
     idHabibation varchar(15) primary key,
@@ -26,15 +21,19 @@ create table Habitation(
     nbrChambre decimal,
     loyer decimal,
     idQuartier varchar(15),
-    descriptionH varchar(250), 
+    descriptionH text, 
     FOREIGN KEY (idType) references TypeH(idTypeH),
     FOREIGN KEY (idQuartier) references Quartier(idQuartier)
 );
+create sequence habseq; start with 1
+
 
 create table Pics(
     idPics varchar(15) primary key,
     nom varchar(100)
-)
+);
+create sequence picsseq start with 1;
+
 
 create table Client(
     idClient varchar(15) primary key,
@@ -43,6 +42,7 @@ create table Client(
     mdp varchar(30),
     numTel varchar(30) unique
 );
+create sequence clientseq start with 1;
 
 create table Admin(
     idAdmin varchar(15) primary key,
@@ -51,4 +51,4 @@ create table Admin(
     mdp varchar(30),
     numTel varchar(30) unique
 );
-
+create sequence adminseq start with 1;
